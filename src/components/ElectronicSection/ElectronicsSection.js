fe3d309f1a84ee281d655a19fa90c8faead1e043
Gagan Agrawal <134318471+gaganagrawal02@users.noreply.github.com>
2024-08-25 19:42:38 +0530

    4 commit

--- src/components/ElectronicSection/ElectronicsSection.js
+++ src/components/ElectronicSection/ElectronicsSection.js
@@ -10,12 +10,12 @@ const ElectronicsSection = () => {
     navigate(link); // Navigate to the specific link when the image is clicked
   };
   const electronicsData = [
-    { id: 1,  imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Laptop Support',link: '/ACRepairPage' } ,
-    { imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Desktop Support' },
-    { imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Macbook support' },
-    { imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Printer Support' },
-    { imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Coding issue' },
-    { imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Component Replacement' },
+    { id: 1,  imgSrc: 'images/macbook.png', text: 'Laptop Support',link: '/ACRepairPage' } ,
+    { imgSrc: 'images/macbook.png', text: 'Desktop Support' },
+    { imgSrc: 'images/macbook.png', text: 'Macbook support' },
+    { imgSrc: 'images/macbook.png', text: 'Printer Support' },
+    { imgSrc: 'images/macbook.png', text: 'Coding issue' },
+    { imgSrc: 'images/macbook.png', text: 'Component Replacement' },
     // { imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Mobile Extended Warranty' },
     // { imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Laptop Complete Care' },
     // { imgSrc: 'https://i.postimg.cc/LXsL8sQk/macbook.png', text: 'Microwave Extended Warranty' },
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,60 +1,3 @@
-// import React , { useState }  from 'react';
-// import './Navbar.css';
-
-
-// import { FaShoppingCart ,FaUser,FaBars, FaTimes} from 'react-icons/fa';
-// // import { FiMenu } from "react-icons/fi";
-// import { Link } from 'react-router-dom';
-
-
-// function Navbar() {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-// const toggleMenu = () => {
-//   setIsMenuOpen(!isMenuOpen);
-// };
-//     return (  
-      
-//         <nav className="navbar  ">
-//           <div className="navbar-left">
-//         <div className="navbar-logo">
-//         <Link to="/">
-//         <img src="https://static.onsitego.com/_next/static/media/onsite-logo-inverse.66c01a54.webp" alt=''/>
-//         </Link>
-//         </div >
-//         <button className="menu-toggle" onClick={toggleMenu}>
-//           {isMenuOpen ? <FaTimes /> : <FaBars />}
-//         </button>
-//         </div>
-//         <ul className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
-    
-//           <li><a href="/">Devices and Plans</a></li>
-    
-//           <li><a href="/">Home Protection</a></li>
-    
-//           <li><a href="/">Activation Plan</a></li>
-        
-//           <li><a href="/">Track Service Request</a></li>
-       
-//         </ul>
-//         <div className={`navbar-sign ${isMenuOpen ? 'open' : ''}`}>
-//         <Link to="/signin" className="sign-in-link">
-//           <div className="sign-icon-wrapper">
-//             <FaUser className="sign-icon" />
-//           </div>
-//           <span>Sign In</span>
-//           </Link>
-        
-//         </div>
-//         <div className="navbar-cart">
-//         <FaShoppingCart className="cart-icon" />
-//       </div>
-//       </nav>
-
-//     );
-// }
-
-// export default Navbar;
 
 import React, { useState } from 'react';
 import { FaShoppingCart, FaUser, FaBars, FaTimes } from 'react-icons/fa';
@@ -76,7 +19,7 @@ const Navbar = () => {
           {isMenuOpen ? <FaTimes /> : <FaBars />}
         </button>
         <div className="navbar-logo">
-          <img src="https://static.onsitego.com/_next/static/media/onsite-logo-inverse.66c01a54.webp" alt="" />
+          <img src="images/Logo.webp" alt="" />
         </div>
       </div>
       <ul className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
